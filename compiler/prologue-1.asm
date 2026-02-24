519ae2c086d1faf4dbb70e15f459f4407b660fbc
yuval9901 <yuvalgon@post.bgu.ac.il>
2026-02-24 17:13:23 +0200

    Add files via upload

---
+++ compiler/prologue-1.asm
@@ -0,0 +1,83 @@
+;;; prologue-1.asm
+;;; The first part of the standard prologue for compiled programs
+;;;
+;;; Programmer: Mayer Goldberg, 2023
+
+%define T_void 				0
+%define T_nil 				1
+%define T_char 				2
+%define T_string 			3
+%define T_closure 			4
+%define T_undefined			5
+%define T_boolean 			8
+%define T_boolean_false 		(T_boolean | 1)
+%define T_boolean_true 			(T_boolean | 2)
+%define T_number 			16
+%define T_integer			(T_number | 1)
+%define T_fraction 			(T_number | 2)
+%define T_real 				(T_number | 3)
+%define T_collection 			32
+%define T_pair 				(T_collection | 1)
+%define T_vector 			(T_collection | 2)
+%define T_symbol 			64
+%define T_interned_symbol		(T_symbol | 1)
+%define T_uninterned_symbol		(T_symbol | 2)
+
+%define SOB_CHAR_VALUE(reg) 		byte [reg + 1]
+%define SOB_PAIR_CAR(reg)		qword [reg + 1]
+%define SOB_PAIR_CDR(reg)		qword [reg + 1 + 8]
+%define SOB_STRING_LENGTH(reg)		qword [reg + 1]
+%define SOB_VECTOR_LENGTH(reg)		qword [reg + 1]
+%define SOB_CLOSURE_ENV(reg)		qword [reg + 1]
+%define SOB_CLOSURE_CODE(reg)		qword [reg + 1 + 8]
+
+%define OLD_RBP 			qword [rbp]
+%define RET_ADDR 			qword [rbp + 8 * 1]
+%define ENV 				qword [rbp + 8 * 2]
+%define COUNT 				qword [rbp + 8 * 3]
+%define PARAM(n) 			qword [rbp + 8 * (4 + n)]
+%define AND_KILL_FRAME(n)		(8 * (2 + n))
+
+%define MAGIC				496351
+
+%macro ENTER 0
+	enter 0, 0
+	and rsp, ~15
+%endmacro
+
+%macro LEAVE 0
+	leave
+%endmacro
+
+%macro assert_type 2
+        cmp byte [%1], %2
+        jne L_error_incorrect_type
+%endmacro
+
+%define assert_void(reg)		assert_type reg, T_void
+%define assert_nil(reg)			assert_type reg, T_nil
+%define assert_char(reg)		assert_type reg, T_char
+%define assert_string(reg)		assert_type reg, T_string
+%define assert_symbol(reg)		assert_type reg, T_symbol
+%define assert_interned_symbol(reg)	assert_type reg, T_interned_symbol
+%define assert_uninterned_symbol(reg)	assert_type reg, T_uninterned_symbol
+%define assert_closure(reg)		assert_type reg, T_closure
+%define assert_boolean(reg)		assert_type reg, T_boolean
+%define assert_integer(reg)		assert_type reg, T_integer
+%define assert_fraction(reg)		assert_type reg, T_fraction
+%define assert_real(reg)		assert_type reg, T_real
+%define assert_pair(reg)		assert_type reg, T_pair
+%define assert_vector(reg)		assert_type reg, T_vector
+
+%define sob_void			(L_constants + 0)
+%define sob_nil				(L_constants + 1)
+%define sob_boolean_false		(L_constants + 2)
+%define sob_boolean_true		(L_constants + 3)
+%define sob_char_nul			(L_constants + 4)
+
+%define bytes(n)			(n)
+%define kbytes(n) 			(bytes(n) << 10)
+%define mbytes(n) 			(kbytes(n) << 10)
+%define gbytes(n) 			(mbytes(n) << 10)
+
+section .data
